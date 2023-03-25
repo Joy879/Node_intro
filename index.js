ea2660eb3b1e1c4a7b53e818a55bcfcf1ecb5d8c
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2023-03-25 23:15:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 name = "Joy Wanjiru"
 language = "Python"
 vision = "Developing full stack ML solutions"
-console.log(`My name is {name}. My preferred programming language is {language} and in one year's time I'd like to be proficient in {vision}`)
+console.log(`My name is ${name}. My preferred programming language is ${language} and in one year's time I'd like to be proficient in ${vision}`)
